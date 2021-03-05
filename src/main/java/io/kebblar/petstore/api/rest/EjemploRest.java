2ad280c1c85d26d07bdc04d9b6698c144c0a969f
garellano <garellano@localhost>
2021-03-05 17:01:21 -0600

    First controller v3

---
+++ src/main/java/io/kebblar/petstore/api/rest/EjemploRest.java
@@ -0,0 +1,24 @@
+package io.kebblar.petstore.api.rest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class EjemploRest {
+
+    @GetMapping(path = "/prueba.json", produces = "application/json; charset=utf-8")
+    public Map<String, String> prueba() throws ServletException {
+        Map<String, String> mapa = new HashMap<>();
+        mapa.put("dato-1", "gus");
+        mapa.put("dato-2", "tavo");
+        return mapa;
+    }
+
+}
